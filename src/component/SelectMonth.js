dd7d66c7a6e9bacc65bd7d6600b538292dd8e121
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2020-11-09 16:50:15 -0300

    feat: :sparkles: fixed horizontal scroll

--- src/component/SelectMonth.js
+++ src/component/SelectMonth.js
@@ -1,31 +1,28 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
+import getScrollPos from './../utils/getScrollPos'
 const SelectMonth = () => {
     const context = useContext(AppContext)
-    const linkRef = useRef()
-    const handleChange = (e) => { 
-        linkRef.current.href = `#month-${e.target.value}`
-        linkRef.current.click()
+    const handleChange = (e) => {
+        let element = document.getElementById('project-scroll')
+        element.scrollLeft = getScrollPos(e.target.value) - 30
     }
 
     return (
-        <>
-            <a style={{ display:"none"}} ref={linkRef} href='#month-01' id='link-ref'>move</a>
-            <select defaultValue={context.currentMonth} onChange={handleChange}>
-                <option value='01'>January</option>
-                <option value='02'>February</option>
-                <option value='03'>March</option>
-                <option value='04'>April</option>
-                <option value='05'>May</option>
-                <option value='06'>June</option>
-                <option value='07'>July</option>
-                <option value='08'>Agost</option>
-                <option value='09'>September</option>
-                <option value='10'>October</option>
-                <option value='11'>November</option>
-                <option value='12'>December</option>
-            </select>
-            </>
+        <select defaultValue={context.currentMonth} onChange={handleChange}>
+            <option value='01'>January</option>
+            <option value='02'>February</option>
+            <option value='03'>March</option>
+            <option value='04'>April</option>
+            <option value='05'>May</option>
+            <option value='06'>June</option>
+            <option value='07'>July</option>
+            <option value='08'>Agost</option>
+            <option value='09'>September</option>
+            <option value='10'>October</option>
+            <option value='11'>November</option>
+            <option value='12'>December</option>
+        </select>
     )
 }
 
